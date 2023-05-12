9ebbd77e79ed8bee5d0efba85463f0046fb3fc5d
Balssh <george.bals25@gmail.com>
2023-05-12 15:11:10 +0300

    a good first config

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,17 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.everforest" },
+  { import = "astrocommunity.indent.indent-blankline-nvim" },
+  { import = "astrocommunity.indent.mini-indentscope" },
+  { import = "astrocommunity.scrolling.mini-animate" },
+  { import = "astrocommunity.motion.harpoon" },
+  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  -- { import = "astrocommunity.pack." },
 }
